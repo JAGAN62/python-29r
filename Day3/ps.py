692b6d2352c96c164d67a474dc9e212a20709036
JAGNA62 <jagan.mamanduru@gamil.com>
2025-02-21 12:18:07 +0530

    day 4 is added

---
+++ Day3/ps.py
@@ -0,0 +1,116 @@
+#positive, negative,zero
+print(' 1) Positive, Negitive or Zero')
+user1 = input('Enter a number: ')
+if user1.isdigit() or (user1[0] == '-' and user1[1:].isdigit()):
+    user11 = int(user1)
+    if user11 < 0:
+        print(f'{user11} is Negative')
+    elif user11 == 0:
+        print(f'{user11} is zero')
+    elif user11 > 0:
+        print(f'{user11} is positive')
+else:
+    print('Invalid input')
+print()
+
+#Even or odd
+print('Even or Odd')
+user2 = input('Enter a number : ')
+if(user2.isdigit()):
+    user22 = int(user2)
+    if(user22 % 2 == 1):
+        print(f'{user22} is odd')
+    else:
+        print(f'{user22} is even')
+else:
+    print('Invalid')
+print()
+
+#vote
+print('vote validation')
+age = int(input('Enter u r Age : '))
+if(age.isdigit()):
+    age1 = int(age)
+    if(age1 > 0 and age1 < 18):
+        print('Not Eligible for vote')
+    elif(age1 >= 18):
+        print('Eligible for vote')
+else:
+    print('Invalid')
+print()
+
+#greater
+n1,n2 = map(int,input('Enter two numbers with space : ').split())
+if (n1 == n2):
+    print('equal')
+print(f'{n1} is greater') if (n1 > n2) else print(f'{n2} is greater')
+
+#marks
+marks = int(input('Enter marks : '))
+print('Pass') if (marks) >=40 else print('Fail')
+
+#days
+day = int(input("Enter a number for days : "))
+if(day == 1):
+    print("Sunday")
+elif(day == 2):
+    print("Monday")
+elif(day == 3):
+    print("Tuesday")
+elif(day == 4):
+    print("Wednesday")
+elif(day == 5):
+    print("Thursday")
+elif(day == 6):
+    print("Friday")
+elif(day == 7):
+    print("Saturday")
+else:
+    print('Invalid')
+
+#calculator
+a,b = map(int,input('Enter two numbers : ').split())
+cal = input('Enter required operation of first three letters : ').lower()
+if(b == 0):
+    print('divison with 0 is not possible')
+def on(cal):
+    if (cal == 'add'):
+        return a+b
+    elif (cal == 'sub'):
+        return a-b
+    elif (cal == 'mul'):
+        return a*b
+    elif (cal == 'div'):
+        return a/b
+    else:
+        return 'invalid'
+print(on(cal))
+
+#months
+mon = int(input("Enter a number for Months : "))
+if(mon == 1):
+    print("January")
+elif(mon == 2):
+    print("February")
+elif(mon == 3):
+    print("March")
+elif(mon == 4):
+    print("April")
+elif(mon == 5):
+    print("May")
+elif(mon == 6):
+    print("June")
+elif(mon == 7):
+    print("July")
+elif(mon == 8):
+    print("August")
+elif(mon == 9):
+    print("September")
+elif(mon == 10):
+    print("October")
+elif(mon == 11):
+    print("November")
+elif(mon == 17):
+    print("December")
+else:
+    print('Invalid')
---
+++ Day4/medium.py
@@ -0,0 +1,84 @@
+'''
+#Write a program to find the greatest of three numbers.
+print('1) Greatest of three numbers')
+a , b , c = input("Enter three nums seperated by space : ").split()
+if a.isdigit() and b.isdigit() and c.isdigit():
+    a1,b1,c1 = int(a),int(b),int(c)
+    if (a1 > b1 ) and (a1 > c1):
+        print(f'{a1} is greater')
+    elif (b1 > a1) and (b1 > c1):
+        print(f'{b1} is greater')
+    else:
+        print(f'{c1} is greater')
+else:
+    print('Invalid input')
+print()
+
+#Check if a year is a leap year
+print('2) check Leap year')
+years = input('Enter a year : ')
+if (years.isdigit()):
+    year = int(years)
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f'{year} is a leap year.')
+    else:
+        print(f'{year} is not a leap year.')
+else:
+    print('Invalid input')
+print()
+
+#classify a character entered by the user as a vowel, consonant, or neither.
+vowles = 'aeiou'
+consonants ='bcdfghjklmnpqrstvwxyz'
+print('3) Classify Characters')
+char = input('Enter a single character : ').lower()
+if (len(char)==1):
+    if(char.isalpha() or not(char.isalnum())):
+        if (char in vowles):
+            print(f" '{char}' is vowle")
+        elif(char in consonants):
+            print(f" '{char}' is consonant")
+        else:
+            print(f" '{char}' is special Character")
+    else:
+        print('Invalid input')
+else:
+    print('Enter single char  only')
+print()
+
+#Calculate the grade  1. 90-100 : Grade A 2. 80-89 : Grade B 3. 70-79 : Grade C 4. <70 : Fail.
+print('4) Grade Calculation')
+user_marks = input('Enter your marks : ')
+if(user_marks.isdigit()):
+    marks = int(user_marks)
+    if(marks >= 90 and marks <= 100):
+        print('Grade A')
+    elif(marks >=80 and marks < 90 ):
+        print('Grade B')
+    elif(marks >=70 and marks < 80):
+        print('Grade C')
+    else:
+        print('Fail')
+else:
+    print("Invalid input")
+print()
+
+
+#Write a program to check if three sides length form a valid triangle.
+print('5) Check valid triangle')
+s1,s2,s3 = input("Enter length of three sides seperated by space : ").split()
+if s1.isdigit() and s2.isdigit() and s3.isdigit():
+    side1,side2,side3 = int(s1),int(s2),int(s3)
+    if((side1+side2) > side3 and (side2+side3) > side1 and (side3+side1) > side2):
+        print(f'Form a valid Triangle with side lengths {side1},{side2},{side3}.')
+    else:
+        print(f'Not form valid Triangle with side lengths {side1},{side2},{side3}.')
+else:
+    print('Invalid Input')
+
+'''
+
+
+
+        
+
